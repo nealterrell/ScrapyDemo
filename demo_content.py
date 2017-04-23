e0b078c296ae92a06fc9d00c322af922f6ea90d3
nealterrell <neal.terrell@csulb.edu>
2017-04-22 18:29:45 -0700

    Consistent string delimiters.

--- demo_content.py
+++ demo_content.py
@@ -3,22 +3,23 @@ import scrapy
 # In demo_titles, our output only had the title of each blog post. What if we wanted
 # the content too?
 
+
 class BlogSpider(scrapy.Spider):
-    name = 'blogspider_content'
-    start_urls = ['https://blog.scrapinghub.com']
+    name = "blogspider_content"
+    start_urls = ["https://blog.scrapinghub.com"]
 
     def parse(self, response):
-        # We don't want to iterate only over the titles anymore; we want to iterate
+        # We don"t want to iterate only over the titles anymore; we want to iterate
         # over the <article> entries.
-        for article in response.css('article'):
+        for article in response.css("article"):
             # Ask this article for its title
-            title = article.css('h2.entry-title')
+            title = article.css("h2.entry-title")
             # Ask this article for its content summary div.
-            content = article.css('div.entry-summary')
-            yield {'title': title.css('a ::text').extract_first(),
-                   # The content div's first <p> has the actual content.
-                   'content': content.css('p').extract_first()}
+            content = article.css("div.entry-summary")
+            yield {"title": title.css("a ::text").extract_first(),
+                   # The content div"s first <p> has the actual content.
+                   "content": content.css("p").extract_first()}
 
-        next_page = response.css('div.prev-post > a ::attr(href)').extract_first()
+        next_page = response.css("div.prev-post > a ::attr(href)").extract_first()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
--- demo_titles.py
+++ demo_titles.py
@@ -1,15 +1,16 @@
 import scrapy
 
+
 # This demo is courtesy of www.scrapy.org
 
 # A Spider is responsible for registering URLs to download, and mapping
 # documents that are downloaded to some data format.
 class BlogSpider(scrapy.Spider):
     # These two fields tell Scrapy some basic info about our spider.
-    name = 'blogspider_titles'
+    name = "blogspider_titles"
     # start_urls contains a list of URLs that will kick off the scraping.
     # Other urls can be discovered and registered as the start_urls are processed.
-    start_urls = ['https://blog.scrapinghub.com']
+    start_urls = ["https://blog.scrapinghub.com"]
 
     # parse is called with an HTML response to a URL that was either in start_urls,
     # or later registered with scrapy.Request.
@@ -24,11 +25,11 @@ class BlogSpider(scrapy.Spider):
 
         # response.css takes a CSS selector, in this case returning any h2 element
         # of the css class "entry-title".
-        for title in response.css('h2.entry-title'):
-            # "a ::text" selects an anchor tag's (<a>) inner text attribute.
+        for title in response.css("h2.entry-title"):
+            # "a ::text" selects an anchor tag"s (<a>) inner text attribute.
             # extract() and extract_first() return the string contained by the
             # given HTML element; otherwise we get a complex object.
-            yield {'title': title.css('a ::text').extract_first()}
+            yield {"title": title.css("a ::text").extract_first()}
 
         # Now that we have parsed all the blog entries on this page, we see if there
         # is a new URL for the spider to crawl.
@@ -37,7 +38,7 @@ class BlogSpider(scrapy.Spider):
         # identified element. In this case, we return all the <a> children of a div
         # with the "prev-post" class. ::attr(href) returns the href="..." attribute
         # of the anchor.
-        next_page = response.css('div.prev-post > a ::attr(href)').extract_first()
+        next_page = response.css("div.prev-post > a ::attr(href)").extract_first()
         if next_page:
             # Tell Scrapy to crawl this new URL, adding it to the crawl queue.
             # The method parse (the one we are in) will be called with the results.
